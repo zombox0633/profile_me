73ae1587d80fed75c48e6abb40827734236b668f
Zombox <ghostzb@outlook.com>
2025-01-15 16:56:50 +0700

    update cv

--- src/components/section/Profile.tsx
+++ src/components/section/Profile.tsx
@@ -1,5 +1,5 @@
 import ImageMe from "../../img/ImageMe.jpg";
-import Resume from "../../cv/ChayathornCV.pdf";
+import Resume from "../../cv/ChayatronMeesilCV_SoftwareEngineer.pdf";
 
 function Profile() {
   return (
